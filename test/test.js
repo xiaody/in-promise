f605a608ef2d3015c2ee151e1c6b15db98109ee4
xiaodongyang <xiaodongyang@meituan.com>
2015-07-14 11:21:05 +0800

    update npm scripts: test & cover

--- test/test.js
+++ test/test.js
@@ -4,6 +4,8 @@ var Promise = require('../')
     , assert = require('assert')
     , noop = function () {}
     , throwE = function () { throw new Error }
+    , promisesAplusTests = require('promises-aplus-tests')
+    , promiseAplusAdapter = require('./aplus')
 
 assert.throws(function () {
     Promise(noop)
@@ -16,3 +18,7 @@ assert.throws(function () {
 assert.doesNotThrow(function () {
     new Promise(throwE)
 })
+
+promisesAplusTests(promiseAplusAdapter, function (err) {
+    // All done; output is in the console. Or check `err` for number of failures.
+});
